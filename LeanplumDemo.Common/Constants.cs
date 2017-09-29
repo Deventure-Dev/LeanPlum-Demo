17c92312c72196648d7f9f19a8635aa3a45a5ad7
Flavius Demian <flavius.demian@deventure.co>
2017-09-29 14:51:55 +0300

    stable version for demo

--- LeanplumDemo.Common/Constants.cs
+++ LeanplumDemo.Common/Constants.cs
@@ -3,10 +3,17 @@ namespace LeanplumDemo.Common
 {
     public class Constants
     {
-        public const string LEANPLUM_APP_ID = "app_AtWaaHCqEHWTSJkClBN8CTyZaiLmUQw2Ww9rBLnFRpI";
+		//OLD ACCOUNT
+		//public const string LEANPLUM_APP_ID = "app_AtWaaHCqEHWTSJkClBN8CTyZaiLmUQw2Ww9rBLnFRpI";
 
-        public const string LEANPLUM_DEV_APP_KEY = "dev_1z5nz8AnzBphkhqE4nvVimEtm7AwNa2LTifoxCcHOlQ";
-        public const string LEANPLUM_PROD_APP_KEY = "prod_YizCIJ0zI7ed4vmZHSoArVDmkr3WtVNlVyqqyC5UqTc";
+		//public const string LEANPLUM_DEV_APP_KEY = "dev_1z5nz8AnzBphkhqE4nvVimEtm7AwNa2LTifoxCcHOlQ";
+		//public const string LEANPLUM_PROD_APP_KEY = "prod_YizCIJ0zI7ed4vmZHSoArVDmkr3WtVNlVyqqyC5UqTc";
+
+		//NEW ACCOUNT
+		public const string LEANPLUM_APP_ID = "app_kM8thQ0RUvRBjZdwa6pbBQ8CjEdw70Onp1j2AsDI2mg";
+
+		public const string LEANPLUM_DEV_APP_KEY = "dev_chPg5UMfQOxW9J3UJjv0WqrdwCVNRD59wBhKy5zqxms";
+		public const string LEANPLUM_PROD_APP_KEY = "prod_feH4BwthFFZRgJCu36hArRHnY569C0zHfcsKmW6oHRM";
 
         public const string RED_COLOR = "red";
 		public const string GREEN_COLOR = "green";
